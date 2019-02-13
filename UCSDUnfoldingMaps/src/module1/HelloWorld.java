a9d8e977e073d7a42958c17c159bde3c527fff13
Linda Perez <rosmery.p.p@gmail.com>
2019-02-13 12:27:09 -0800

    Finished Second Week Course I

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -6,6 +6,7 @@ import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
 /** HelloWorld
@@ -46,7 +47,7 @@ public class HelloWorld extends PApplet
 		this.background(200, 200, 200);
 		
 		// Select a map provider
-		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+		AbstractMapProvider provider = new Microsoft.AerialProvider();
 		// Set a zoom level
 		int zoomLevel = 10;
 		
@@ -66,17 +67,26 @@ public class HelloWorld extends PApplet
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
 		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		
+		
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
+	   
 		
 		// This line makes the map interactive
-		MapUtils.createDefaultEventDispatcher(this, map1);
+		//MapUtils.createDefaultEventDispatcher(this, map1);
+		
+		
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
-
+		map2 = new UnfoldingMap(this, 420, 50, 350, 500, provider);
+		 //37.4076922,-121.998556,17z
+	    map2.zoomAndPanTo(zoomLevel, new Location(37.4076922, -121.998556));
+	    
+	    MapUtils.createDefaultEventDispatcher(this, map1,map2);
 	}
 
 	/** Draw the Applet window.  */
@@ -84,6 +94,8 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
+		
 	}
 
 	
