185faf72a3549c5f0f244bbe38c3b3375441812b
Linda Perez <rosmery.p.p@gmail.com>
2019-02-13 16:02:06 -0800

    Scale of brightness of the Sun

--- UCSDUnfoldingMaps/src/MyContributions/SunCycle.java
+++ UCSDUnfoldingMaps/src/MyContributions/SunCycle.java
@@ -25,18 +25,34 @@ public class SunCycle extends PApplet {
 	}
 	
 	public void draw(){
-		// set an image 
+		// set an image
 		image(img,0,0);
 		//draw a circle
-		ellipse(100,80,80,80);
+		
 		//fill color
 		//change color according to time
-		int s=0,c=0;
+		int [] rgb = sunColorSet(second());
+		fill(rgb[0],rgb[1],rgb[2]);
+		ellipse(100,80,80,80);
+		
+	/*	int s=0,c=0;
 		while(c<=10){
 			fill(255,255,(s+15)*3);
 			s=LocalTime.now().getSecond();
 			repaint();
 			c++;
-		}
+		}*/
+		
+	}
+	public int[] sunColorSet(float seg){
+		int [] rgb=new int[3];
+		float diff30 = Math.abs(30-seg);
+		float ratio = diff30/30;
+		//scale second from 60 to 0
+		rgb[0]=(int)(255*ratio);
+		rgb[1]=(int)(255*ratio);
+		rgb[2]=0;
+				
+	return rgb;	
 	}
 }
